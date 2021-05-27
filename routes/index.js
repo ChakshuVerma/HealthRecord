4c3675e41e6b4cf873b1ebd3b1111f0e31d0d68e
ChakshuVerma <69800010+ChakshuVerma@users.noreply.github.com>
2021-05-27 16:01:56 +0530

    Added Homepage: cls

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const route = require('express').Router()
 const path  = require('path')
 
-route.get('/', (req,res) => res.render('graph'))
+route.get('/', (req,res) => res.render('welcome'))
+route.get('/status', (req,res) => res.render('graph'))
 route.get('/addData', (req,res) => res.render('add_data'))
 
-exports = module.exports = route; 
+exports = module.exports = route;  
