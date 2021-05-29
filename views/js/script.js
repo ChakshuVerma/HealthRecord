ab586ee314de7d7a9bcad264ac31c2ae1e99c61a
ChakshuVerma <69800010+ChakshuVerma@users.noreply.github.com>
2021-05-29 13:59:10 +0530

    Flexibility adjustment

--- views/js/script.js
+++ views/js/script.js
@@ -87,7 +87,7 @@ function showdata(event) {
         },3000)
     }
     else{
-        document.querySelector('container').style.overflow = 'scroll'
+        document.querySelector('.container').style.overflow = 'auto'
         var button_id = event.target.id;
         var dataset = [], labels = [];
         var LineColor,data; 
