2d3df7fe665e7f8f0821bb78b67687e6b490611b
ChakshuVerma <69800010+ChakshuVerma@users.noreply.github.com>
2021-06-05 11:24:16 +0530

    Flexibility adjustment

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const path  = require('path')
 route.get('/', (req,res) => res.render('welcome'))
 route.get('/status', (req,res) => res.render('graph'))
 route.get('/addData', (req,res) => res.render('add_data'))
+route.get('/faq', (req,res) => res.render('faq'))
 
 exports = module.exports = route;  
